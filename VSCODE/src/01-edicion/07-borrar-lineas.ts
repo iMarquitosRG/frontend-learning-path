b9a9385ea0e99d42bf0767bb995126f99e3844d5
Marcos Rivera <marcosrivera@Marcoss-MacBook-Pro.local>
2025-08-09 12:12:40 -0600

    Initial commit: Estructura inicial de carpetas del curso

---
+++ VSCODE/src/01-edicion/07-borrar-lineas.ts
@@ -0,0 +1,29 @@
+//*
+//*    Objetivo:
+//*        Comentar un bloque de código
+//*
+//*    Tips:
+//*        ⇧ ⌘ K
+//*        Ctrl + Shift + K
+//*
+//*    PRO:
+//*        Seleccionar todas las ocurrencias de la selección
+//*        ⇧ ⌘ L
+//*        Ctrl + Shift + L
+//*
+
+let  = ':)';
+let Borrame = ':(';
+
+ = '1';
+Borrame    = 'a';
+ = '1';
+Borrame    = 'a';
+
+
+// Resultado final (sin los comentarios claro.)
+
+// let  = ':)';
+
+//  = '1';
+//  = '1';
